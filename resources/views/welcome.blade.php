7f25571ec4755349881f8b47d0bb6ddb9a20d719
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2020-06-18 19:41:08 +0700

    Add UI for Home Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Data Kendaraan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -62,8 +62,105 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="https://www.umy.ac.id/xmlrpc.php">
+	<link rel="alternate" hreflang="en-us" href="http://www.umy.ac.id/en" />
+<link rel="alternate" hreflang="id-id" href="http://www.umy.ac.id" />
+<link rel='dns-prefetch' href='//maxcdn.bootstrapcdn.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Universitas Muhammadiyah Yogyakarta &raquo; Feed" href="https://www.umy.ac.id/feed" />
+<link rel="alternate" type="application/rss+xml" title="Universitas Muhammadiyah Yogyakarta &raquo; Umpan Komentar" href="https://www.umy.ac.id/comments/feed" />
+<link rel="alternate" type="application/rss+xml" title="Universitas Muhammadiyah Yogyakarta &raquo; Home Umpan Komentar" href="https://www.umy.ac.id/8169-2/feed" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/www.umy.ac.id\/wp-includes\/js\/wp-emoji-release.min.js"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='bootstrap-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/css/bootstrap.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='jasny-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/3rdparty/jasny/css/jasny-bootstrap.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='icomoon-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/fonts/icomoon/style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='umy16-style-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='bxslider-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/css/jquery.bxslider.css' type='text/css' media='all' />
+<link rel='stylesheet' id='fontawesome-css'  href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-block-library-css'  href='https://www.umy.ac.id/wp-includes/css/dist/block-library/style.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='news-css'  href='https://www.umy.ac.id/wp-content/plugins/simple-news/css/news-min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='siteorigin-panels-front-css'  href='https://www.umy.ac.id/wp-content/plugins/siteorigin-panels/css/front-flex.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='wpml-legacy-horizontal-list-0-css'  href='//www.umy.ac.id/wp-content/plugins/sitepress-multilingual-cms/templates/language-switchers/legacy-list-horizontal/style.css' type='text/css' media='all' />
+<style id='wpml-legacy-horizontal-list-0-inline-css' type='text/css'>
+.wpml-ls-statics-shortcode_actions{background-color:#eeeeee;}.wpml-ls-statics-shortcode_actions, .wpml-ls-statics-shortcode_actions .wpml-ls-sub-menu, .wpml-ls-statics-shortcode_actions a {border-color:#cdcdcd;}.wpml-ls-statics-shortcode_actions a {color:#444444;background-color:#ffffff;}.wpml-ls-statics-shortcode_actions a:hover,.wpml-ls-statics-shortcode_actions a:focus {color:#000000;background-color:#eeeeee;}.wpml-ls-statics-shortcode_actions .wpml-ls-current-language>a {color:#444444;background-color:#ffffff;}.wpml-ls-statics-shortcode_actions .wpml-ls-current-language:hover>a, .wpml-ls-statics-shortcode_actions .wpml-ls-current-language>a:focus {color:#000000;background-color:#eeeeee;}
+</style>
+<link rel='stylesheet' id='wp-pagenavi-css'  href='https://www.umy.ac.id/wp-content/plugins/wp-pagenavi/pagenavi-css.css' type='text/css' media='all' />
+<link rel='stylesheet' id='tjd_slider_style-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/tjd-plugins/tjd-slider/assets/css/style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='tjd_slider_style_bxslider-css'  href='https://www.umy.ac.id/wp-content/themes/umy-mobile/tjd-plugins/tjd-slider/assets/jquery.bxslider/jquery.bxslider.css' type='text/css' media='all' />
+<!--[if lt IE 9]>
+<link rel='stylesheet' id='vc_lte_ie9-css'  href='https://www.umy.ac.id/wp-content/plugins/js_composer/assets/css/vc_lte_ie9.min.css' type='text/css' media='screen' />
+<![endif]-->
+<script type='text/javascript' src='https://www.umy.ac.id/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='https://www.umy.ac.id/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<link rel='https://api.w.org/' href='https://www.umy.ac.id/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://www.umy.ac.id/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://www.umy.ac.id/wp-includes/wlwmanifest.xml" /> 
+<link rel="canonical" href="https://www.umy.ac.id/" />
+<link rel='shortlink' href='https://www.umy.ac.id/' />
+<link rel="alternate" type="application/json+oembed" href="https://www.umy.ac.id/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.umy.ac.id%2F" />
+<link rel="alternate" type="text/xml+oembed" href="https://www.umy.ac.id/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.umy.ac.id%2F&#038;format=xml" />
+<meta name="generator" content="WPML ver:4.0.4 stt:1,25;" />
+
+<style type="text/css" id="breadcrumb-trail-css">.breadcrumbs .trail-browse,.breadcrumbs .trail-items,.breadcrumbs .trail-items li {display: inline-block;margin:0;padding: 0;border:none;background:transparent;text-indent: 0;}.breadcrumbs .trail-browse {font-size: inherit;font-style:inherit;font-weight: inherit;color: inherit;}.breadcrumbs .trail-items {list-style: none;}.trail-items li::after {content: "\002F";padding: 0 0.5em;}.trail-items li:last-of-type::after {display: none;}</style>
+<meta name="generator" content="Powered by WPBakery Page Builder - drag and drop page builder for WordPress."/>
+                <style type="text/css" media="all"
+                       id="siteorigin-panels-layouts-head">/* Layout 8169 */ #pgc-8169-0-0 { width:100%;width:calc(100% - ( 0 * 30px ) ) } #pl-8169 #panel-8169-0-0-0 , #pl-8169 #panel-8169-0-0-1 {  } #pl-8169 .so-panel { margin-bottom:30px } #pl-8169 .so-panel:last-child { margin-bottom:0px } @media (max-width:780px){ #pg-8169-0.panel-no-style, #pg-8169-0.panel-has-style > .panel-row-style { -webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column } #pg-8169-0 > .panel-grid-cell , #pg-8169-0 > .panel-row-style > .panel-grid-cell { width:100%;margin-right:0 } #pl-8169 .panel-grid-cell { padding:0 } #pl-8169 .panel-grid .panel-grid-cell-empty { display:none } #pl-8169 .panel-grid .panel-grid-cell-mobile-last { margin-bottom:0px }  } </style><noscript><style> .wpb_animate_when_almost_visible { opacity: 1; }</style></noscript>	<link rel="stylesheet" type="text/css" href="https://www.umy.ac.id/wp-content/themes/umy-mobile/responsive.css">
+	<link rel="shortcut icon" href="https://www.umy.ac.id/wp-content/themes/umy-mobile/images/favicon.ico" type="image/x-icon">
+
+
     </head>
-    <body>
+    <body class="home page-template-default page page-id-8169 siteorigin-panels siteorigin-panels-before-js siteorigin-panels-home wpb-js-composer js-comp-ver-6.0.5 vc_responsive">
+    <div class="canvas">
+	<div class="container">	
+		<div class="row search-top search"> 
+			<div class="col-md-12">
+				<form method="get" id="searchform" action="https://www.umy.ac.id/">	
+					<div id="input" class="input-group">
+						<input type="text" placeholder="Search: Type text then press enter" name="s" id="s" />
+						<div class="input-group-btn">
+							<span class="btn btn-default visible-xs visible-sm"><i aria="hidden" class="fa fa-search"></i></span>
+						</div>
+						<input type="hidden" id="searchsubmit" /> 
+					</div>
+				</form>	
+			
+			</div>
+		</div>
+        <div class="band header">			
+			<div class="row no-gutter">
+				<div class="col-sm-7 col-md-5 col-xs-8">	
+					<button type="button" class="navbar-toggle" data-toggle="offcanvas" data-recalc="false" data-target=".navmenu" data-canvas=".canvas">
+						<span class="icon-bar top-bar"></span>
+						<span class="icon-bar middle-bar"></span>
+						<span class="icon-bar bottom-bar"></span>
+					</button>
+					
+					<h1 class="logo hidden-xs"><a href="https://www.umy.ac.id"><img src="https://www.umy.ac.id/wp-content/themes/umy-mobile/images/logo.png" width="269" height="49"/></a></h1>
+						
+					<h1 class="logo visible-xs"><a href="https://www.umy.ac.id"><img src="https://www.umy.ac.id/wp-content/themes/umy-mobile/images/logo-mobile.png" /></a></h1>
+					
+										
+					
+				</div>	
+                
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -78,23 +175,41 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            
         </div>
+        <div class="col-sm-5 col-md-7 col-xs-4">				
+					
+					
+				
+				</div>
+			</div>			
+		</div>
+
+		<div class="band navigation hidden-xs hidden-sm">
+				<div class="menu-header-container">
+											
+					<ul id="menu-header" class="nav navbar-nav main-nav navbar-right">
+						<li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>
+                        <li ><a ><b style="color: #FBC800">======</b></a></li>   
+                                                                                         					
+						
+					</ul>
+									</div>		
+		</div>
+        <div class="band slider">			
+		<div class="slider_home">				
+				<div class="slider-item">
+				<a><img  width="1140" height="300" style="background-color: #065139" /><p style="position: absolute;top: 110px;z-index: 2;color: white;padding-left: 400px;font-size: 40px">Data Kendaraan</p></a>
+									</div>														
+			
+            
+			</div>	
+           
+           
     </body>
 </html>
